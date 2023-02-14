dd2f2898137a37312fec2921308c138283291655
Joseph Ugiagbe <blessedjoe2000@yahoo.com>
2023-02-14 21:06:04 +0000

    route created

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import express from "express";
+import router from "./routes/photoRoutes.js";
 
 const app = express();
 
 const port = 3000;
 
-app.use("/", (req, res) => {
-  res.status(200).json({ message: "welcome to the Unsplash API" });
-});
+app.use("/api/photos", router);
 
 app.listen(port || process.env.PORT, () => {
   console.log(`Listening to port ${port}`);
