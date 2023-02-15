e3055a04331d414a16700d1dc043bd69b88602b2
Joseph Ugiagbe <blessedjoe2000@yahoo.com>
2023-02-15 00:55:34 +0000

    API Get Routes: Return Unsplash Photos

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -15,3 +15,14 @@ export const getPhotos = async (req, res) => {
     res.status(500).json({ message: "Server error. Please try again later." });
   }
 };
+
+export const getPhotosById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await axios.get(`${BASE_URL}photos?client_id=${access_key}`);
+    const photo = result.data.find((item) => item.id === id);
+    res.status(200).json(photo);
+  } catch (error) {
+    res.status(500).json({ message: "Server error. Please try again later." });
+  }
+};
--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getPhotos } from "../controllers/photoController.js";
+import { getPhotos, getPhotosById } from "../controllers/photoController.js";
 
 const router = express.Router();
 router.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Unsplash API!" });
 });
 router.get("/api/photos", getPhotos);
+
+router.get("/api/photos/:id", getPhotosById);
 
 export default router;
